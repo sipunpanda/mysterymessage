2a29743b8722f3340a1b6a800212a14cdbe469d6
sipunpanda <sipunpanda26@gmail.com>
2025-07-15 18:25:22 +0530

    Data base connection and Email functionality Setup

---
+++ src/Schemas/signUpSchema.ts
@@ -0,0 +1,13 @@
+import {z} from 'zod'
+
+export const usernameValidation = z
+    .string()
+    .min(3,"Username Must be atleast 3 Characters")
+    .min(20,"Username Must be atleast 20 Characters")
+    .regex( /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/, "username must not contain special character")
+
+    export const signUpSchema = z.object({
+        username: usernameValidation,
+        email: z.string().email({message: "Invalid email address"}),
+        password: z.string().min(6,{message: "Invalid email address"})
+    })
---
+++ src/model/user.ts
@@ -0,0 +1,70 @@
+import mongoose, { Schema, Document } from "mongoose"
+import { Interface } from "readline";
+
+
+export interface Message extends Document{
+    content: string;
+    createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        required: true,
+        default:Date.now
+    }
+})
+
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username:{
+        type: String,
+        required: [true, "Username is required"],
+        trim:true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        trim:true,
+        unique: true,
+        match:[/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/, "Please use a Valid Email"]
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode:{
+        type: String,
+        required: [true, "Verify code is required"],
+    },
+    verifyCodeExpiry:{
+        type: Date,
+        required: true,
+        default:Date.now
+    },
+    isVerified:{
+        type:Boolean,
+        default: false
+    },
+    messages: [MessageSchema]
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User> || mongoose.model<User>("User", UserSchema))
+
+export default UserModel
